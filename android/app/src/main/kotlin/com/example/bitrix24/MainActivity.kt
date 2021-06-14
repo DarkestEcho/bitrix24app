d0b1c3c2a08ab45274bac0c2c8b6de08ba5c7f46
Dmitriy Samarin <samarin99@gmail.com>
2021-06-14 19:01:52 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bitrix24/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bitrix24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Bitrix24App());
+}
+
+class Bitrix24App extends StatelessWidget {
+  const Bitrix24App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
